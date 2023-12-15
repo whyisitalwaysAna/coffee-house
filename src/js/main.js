a09e4187cd662f82fb3f7e86c1d908f54e2d2879
dracainna <annasafonova.info@gmail.com>
2023-12-15 20:51:30 +0300

    fix: fix scroll for iOS

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,10 @@ window.addEventListener('DOMContentLoaded', function() {
 let buttonHamburger = document.querySelector('.hamburger-wrapper');
 let modalHamburger = document.getElementById('nav-hamburger');
 let body = document.querySelector('body');
+let bodyActive = document.querySelector('.body-wrapper');
 let liHamburger = document.querySelectorAll('.li-hamburger');
 let spanHamburgerOne = document.querySelector('.span-1');
 let spanHamburgerTwo = document.querySelector('.span-2');
-
 
 buttonHamburger.addEventListener('click', function(event) {
 
@@ -18,10 +18,12 @@ buttonHamburger.addEventListener('click', function(event) {
     spanHamburgerOne.classList.toggle('span-1-open');
     spanHamburgerTwo.classList.toggle('span-2-open');
     body.classList.toggle('overflow');
+    bodyActive.classList.toggle('body-wrapper-active');
 
     liHamburger.forEach(elem => {
         elem.addEventListener('click', function() {
             body.classList.remove('overflow');
+            bodyActive.classList.remove('body-wrapper-active')
             modalHamburger.classList.remove('nav-visible');
         });
     });
